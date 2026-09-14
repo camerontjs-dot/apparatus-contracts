61cd0c111a9e92617b40b1f759bd14f83a41e176
Cameron Sanderson <camerontjs@gmail.com>
2026-09-13 23:58:05 -0400

    promotion: satisfy RC2 adapter lint without semantic change

--- validators/contract_c_rc2.py
+++ validators/contract_c_rc2.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import copy
 import hashlib
 import json
-from typing import Any, Iterable, Mapping
+from collections.abc import Iterable, Mapping
+from typing import Any
 
 from . import contract_c_rc1_frozen as RC1
 
